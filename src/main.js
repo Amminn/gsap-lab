ff4a2380e4902c4469716a8ede159942cb5227ea
Amin <vrb.nnn@gmail.com>
2025-06-05 16:11:03 +0100

    feat: more progress

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
-console.log('test');
+import { gsap } from "gsap";
+
+
+// gsap.from("h1", {
+//   x: -100,
+// })
+
+// gsap.to("h1", {
+//   duration: 2,
+//   x: 0,
+//   ease: "bounce.out"
+// })
+
+gsap.fromTo('h1', {
+  x: -100,
+  opacity: 0,
+}, {
+  duration: 2,
+  x: 100,
+  opacity: 1,
+  ease: "bounce.out"
+})
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.to(".box1", {
+    y: 100,
+    duration: 2,
+    repeat: -1,
+    yoyo: true
+  })
+})
