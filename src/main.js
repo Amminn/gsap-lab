c9d20057e35959e982ce6961898cab48c527af4d
Amin <vrb.nnn@gmail.com>
2025-06-08 23:58:04 +0100

    feat: include gsap dep

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('test');
+import { gsap } from "gsap";
+    
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(GSDevTools);
+
+gsap.to("h1", {
+  x: 100,
+  duration: 1
+})
