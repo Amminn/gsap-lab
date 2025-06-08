d830cd63547e8b5d2dd8775ac9060269e6d21df8
Amin <vrb.nnn@gmail.com>
2025-06-08 18:37:57 +0100

    feat: stagger testing

--- src/main.js
+++ src/main.js
@@ -21,11 +21,20 @@ gsap.fromTo('h1', {
   ease: "bounce.out"
 })
 
+
+
 document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.to(".box1", {
+  gsap.to(".box", {
     y: 100,
+    stagger: {
+      each: 0.1,
+      from: 'end', // from where will the animation start | index
+      // grid: 'auto', // 
+      ease: 'power2.inOut',
+      repeat: -1 // Repeats immediately, not waiting for the other staggered animations to finish
+    },
     duration: 2,
-    repeat: -1,
-    yoyo: true
+    // repeat: -1,
+    // yoyo: true
   })
 })
