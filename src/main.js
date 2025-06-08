e9aef7b2d0808b52f079af7760a227febe0c1bfb
Amin <vrb.nnn@gmail.com>
2025-06-08 23:30:35 +0100

    feat: test some callbacks

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-console.log('test');
+import { gsap } from "gsap";
+
+// the callback function passed to addEventListener expects an Event object as its argument
+addEventListener('DOMContentLoaded', (event) => {
+  let t1 = gsap.timeline();
+
+  t1.to("#heading", {
+    opacity: 1,
+    y: 0,
+    duration: 0.7,
+  }, "+=0.5")
+  .to("p", {
+    opacity: 1,
+    y: 0,
+    duration: 0.7,
+    onComplete: () => {
+      console.log('second is complete')
+    }
+  })
+  .to(".buttons",{
+    opacity: 1,
+    y: 0,
+    duration: 0.7,
+    onStart: () => {
+      console.log('third just started')
+    }
+  }, "<")
+
+})
