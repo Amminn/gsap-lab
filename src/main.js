17a7077362e28c43847a77c560191d072c66073f
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-09 14:49:57 +0100

    feat: test how to prevent FOUC

--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
 import { gsap } from "gsap";
-    
+
 import { GSDevTools } from "gsap/GSDevTools";
 
 addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(GSDevTools);
-  
-  gsap.to("h1", {
-    x: 100,
-    duration: 1
+
+  let t1 = gsap.timeline();
+
+  t1.from("h1", {
+    opacity: 0,
+    y: 100,
+    duration: 1,
   });
-  
+  t1.from("p", {
+    opacity: 0,
+    y: 100,
+    duration: 1,
+    // immediateRender: false,
+  });
+  t1.from(".buttons", {
+    opacity: 0,
+    y: 100,
+    duration: 1,
+  });
+  t1.from("h1", {
+    opacity: 0,
+    y: 100,
+    duration: 1,
+    immediateRender: false,
+  });
+
   GSDevTools.create();
 });
