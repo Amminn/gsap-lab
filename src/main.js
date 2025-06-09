c82f795eb398e2d54436ba95a4a2a1dbd6981cea
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-09 15:06:57 +0100

    feat: try matchMedia with error

--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ import { GSDevTools } from "gsap/GSDevTools";
 addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(GSDevTools);
 
+  let mm = gsap.matchMedia();
+
+  mm.add("(min-width: 700px)"),
+    () => {
+      gsap.to(".buttons", {
+        rotate: 360,
+        repeat: -1,
+        duration: 2,
+        ease: "none",
+      });
+    };
+
   GSDevTools.create();
 });
-
-// autoAlpha
-// using fromTo
