5a4c639e533e5b2d40784ef3b605ed2034fad3b2
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-09 14:51:10 +0100

    init: clean not needed code

--- src/main.js
+++ src/main.js
@@ -5,30 +5,8 @@ import { GSDevTools } from "gsap/GSDevTools";
 addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(GSDevTools);
 
-  let t1 = gsap.timeline();
-
-  t1.from("h1", {
-    opacity: 0,
-    y: 100,
-    duration: 1,
-  });
-  t1.from("p", {
-    opacity: 0,
-    y: 100,
-    duration: 1,
-    // immediateRender: false,
-  });
-  t1.from(".buttons", {
-    opacity: 0,
-    y: 100,
-    duration: 1,
-  });
-  t1.from("h1", {
-    opacity: 0,
-    y: 100,
-    duration: 1,
-    immediateRender: false,
-  });
-
   GSDevTools.create();
 });
+
+// autoAlpha
+// using fromTo
