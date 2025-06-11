a363959454451dd28ab9556300a398d3b52b70e0
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-11 01:19:58 +0100

    feat(WIP): testing with scrollTrigger

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import { gsap } from "gsap";
 
 import { GSDevTools } from "gsap/GSDevTools";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 addEventListener("DOMContentLoaded", () => {
-  gsap.registerPlugin(GSDevTools);
+  gsap.registerPlugin(GSDevTools, ScrollTrigger);
 
-  GSDevTools.create();
+  gsap.to(".card-img", {
+    duration: 10,
+    rotate: 360,
+    scrollTrigger: {
+      trigger: "#container",
+      start: "top 20%",
+      end: "bottom",
+      markers: true,
+      scrub: true,
+      pin: ".card-img",
+    },
+  });
+  // GSDevTools.create();
 });
