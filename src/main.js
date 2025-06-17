1d316604b574cb516c62a5037ee90922b13cbba0
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-17 15:58:52 +0100

    feat: add tawilind css

--- src/main.js
+++ src/main.js
@@ -4,19 +4,18 @@ import { GSDevTools } from "gsap/GSDevTools";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 addEventListener("DOMContentLoaded", () => {
-  gsap.registerPlugin(GSDevTools, ScrollTrigger);
-
-  gsap.to(".card-img", {
-    duration: 10,
-    rotate: 360,
-    scrollTrigger: {
-      trigger: "#container",
-      start: "top 20%",
-      end: "bottom",
-      markers: true,
-      scrub: true,
-      pin: ".card-img",
-    },
-  });
+  // gsap.registerPlugin(GSDevTools, ScrollTrigger);
+  // gsap.to(".card-img", {
+  //   duration: 10,
+  //   rotate: 360,
+  //   scrollTrigger: {
+  //     trigger: "#container",
+  //     start: "top 20%",
+  //     end: "bottom",
+  //     markers: true,
+  //     scrub: true,
+  //     pin: ".card-img",
+  //   },
+  // });
   // GSDevTools.create();
 });
