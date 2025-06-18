dd653ed62495e1948e31876a6cc25bddeb4c9a0e
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-18 23:10:37 +0100

    feat: add horizontal scroll

--- src/main.js
+++ src/main.js
@@ -5,46 +5,93 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(GSDevTools, ScrollTrigger);
-  // gsap.to(".card-img", {
-  //   duration: 10,
-  //   rotate: 360,
-  //   scrollTrigger: {
-  //     trigger: "#container",
-  //     start: "top 20%",
-  //     end: "+=500",
-  //     markers: true,
-  //     // scrub: true,
-  //     // pin: ".card-img",
-  //   },
-  // });
 
-  const tween = gsap.to("#box", {
+  const slides = gsap.utils.toArray(".slide");
+
+  let scrollTween = gsap.to(slides, {
+    xPercent: -100 * (slides.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#app",
+      start: "top top",
+      scrub: 1,
+      pin: true,
+    },
+  });
+
+  gsap.to("#yellow-box", {
     rotate: 360,
-    duration: 2,
-    paused: true, // <-- IMPORTANT
+    x: 200,
+    y: 200,
+    scrollTrigger: {
+      trigger: "#yellow-container",
+      start: "top center",
+      end: "+=500",
+      scrub: true,
+      markers: true,
+      containerAnimation: scrollTween,
+      onEnter: () => {
+        console.log("enter");
+      },
+      onLeave: () => {
+        console.log("leave");
+      },
+      onEnterBack: () => {
+        console.log("enter back");
+      },
+      onLeaveBack: () => {
+        console.log("leave back");
+      },
+    },
+  });
+  gsap.to("#red-box", {
+    rotate: 360,
+    x: -200,
+    y: 200,
+    scrollTrigger: {
+      trigger: "#yellow-container",
+      start: "top center",
+      end: "+=500",
+      scrub: true,
+      markers: true,
+      containerAnimation: scrollTween,
+      onEnter: () => {
+        console.log("enter");
+      },
+      onLeave: () => {
+        console.log("leave");
+      },
+      onEnterBack: () => {
+        console.log("enter back");
+      },
+      onLeaveBack: () => {
+        console.log("leave back");
+      },
+    },
   });
 
   // 2. Create the ScrollTrigger and link it to the paused tween
-  ScrollTrigger.create({
-    animation: tween,
-    trigger: "#box",
-    start: "top 80%",
-    end: "bottom 20%",
-    toggleActions: "play pause resume reset",
-    onEnter: () => {
-      console.log("enter");
-    },
-    onLeave: () => {
-      console.log("leave");
-    },
-    onEnterBack: () => {
-      console.log("enter back");
-    },
-    onLeaveBack: () => {
-      console.log("leave back");
-    },
-    markers: true,
-  });
+  // ScrollTrigger.create({
+  //   animation: tween,
+  //   trigger: "#box",
+  //   start: "top 80%",
+  //   end: "bottom 20%",
+  //   toggleActions: "play pause resume reset",
+  //   scrub: 1,
+  //   onEnter: () => {
+  //     console.log("enter");
+  //   },
+  //   onLeave: () => {
+  //     console.log("leave");
+  //   },
+  //   onEnterBack: () => {
+  //     console.log("enter back");
+  //   },
+  //   onLeaveBack: () => {
+  //     console.log("leave back");
+  //   },
+  //   markers: true,
+  // });
 
   // onEnter, onLeave, onEnterBack, onLeaveBack;
 
