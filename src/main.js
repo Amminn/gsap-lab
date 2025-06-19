2c771b6e213c2dd43c6954ac3d874c257a23f4ae
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-19 22:26:59 +0100

    feat: add scroll pause feature

--- src/main.js
+++ src/main.js
@@ -1,21 +1,40 @@
 import { gsap } from "gsap";
 
-import { GSDevTools } from "gsap/GSDevTools";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 
-addEventListener("DOMContentLoaded", () => {
-  // gsap.registerPlugin(GSDevTools, ScrollTrigger);
-  // gsap.to(".card-img", {
-  //   duration: 10,
-  //   rotate: 360,
-  //   scrollTrigger: {
-  //     trigger: "#container",
-  //     start: "top 20%",
-  //     end: "bottom",
-  //     markers: true,
-  //     scrub: true,
-  //     pin: ".card-img",
-  //   },
-  // });
-  // GSDevTools.create();
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+  ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 2,
+  });
+
+  const smoother = ScrollSmoother.create({
+    smooth: 2,
+  });
+
+  const button = document.getElementById("button");
+  const button2 = document.getElementById("button2");
+  const box1 = document.getElementById("box-1");
+  const box2 = document.getElementById("box-2");
+
+  box1.addEventListener("click", () => {
+    smoother.paused(true);
+    console.log(smoother);
+  });
+
+  box2.addEventListener("click", () => {
+    smoother.paused(false);
+  });
+
+  button.addEventListener("click", () => {
+    smoother.scrollTo(".box-c", true, "center, center");
+  });
+
+  button2.addEventListener("click", () => {
+    smoother.scrollTop(0); // one note about this scrollTop, it doesn't have smooth scroll.
+  });
 });
