250d90f2fb4600290c8aaccd36c2299432a36129
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-19 22:56:19 +0100

    feat: add data-speed and data-lag

--- src/main.js
+++ src/main.js
@@ -14,27 +14,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   const smoother = ScrollSmoother.create({
     smooth: 2,
-  });
-
-  const button = document.getElementById("button");
-  const button2 = document.getElementById("button2");
-  const box1 = document.getElementById("box-1");
-  const box2 = document.getElementById("box-2");
-
-  box1.addEventListener("click", () => {
-    smoother.paused(true);
-    console.log(smoother);
-  });
-
-  box2.addEventListener("click", () => {
-    smoother.paused(false);
-  });
-
-  button.addEventListener("click", () => {
-    smoother.scrollTo(".box-c", true, "center, center");
-  });
-
-  button2.addEventListener("click", () => {
-    smoother.scrollTop(0); // one note about this scrollTop, it doesn't have smooth scroll.
+    effects: true,
   });
 });
