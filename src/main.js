4cd0722b1e79f143b9a2f44ff213cd2641efa76b
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-20 19:59:10 +0100

    init: clean file to test textSplit

--- src/main.js
+++ src/main.js
@@ -1,19 +1,3 @@
 import { gsap } from "gsap";
 
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-  ScrollSmoother.create({
-    wrapper: "#smooth-wrapper",
-    content: "#smooth-content",
-    smooth: 2,
-  });
-
-  const smoother = ScrollSmoother.create({
-    smooth: 2,
-    effects: true,
-  });
-});
+document.addEventListener("DOMContentLoaded", (event) => {});
