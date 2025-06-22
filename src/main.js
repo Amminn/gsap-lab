8aab21545e13fbaab819e29c424574e22459b7ca
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-22 17:20:40 +0100

    feat: add simple textSplit animation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,30 @@
 import { gsap } from "gsap";
 
-document.addEventListener("DOMContentLoaded", (event) => {});
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.fonts.ready.then(() => {
+    // wait for fonts to load, then do the splitting to avoid errors
+    const split = SplitText.create("h1", {
+      type: "lines",
+      autoSplit: true,
+      mask: true,
+      ignore: "span",
+    });
+
+    gsap.from(split.lines, {
+      opacity: 0,
+      y: 60,
+      stagger: {
+        each: 0.1,
+      },
+      ease: "power2.inOut",
+      duration: 1,
+      // onComplete: () => {
+      //   split.revert(); // once all the animation is done, the html will get back to it's original value
+      // },
+    });
+  });
+});
