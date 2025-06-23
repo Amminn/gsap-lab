a76a0571499b5feee6faab780e8cb287c68b540a
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-23 02:02:38 +0100

    feat: try keyframs

--- src/main.js
+++ src/main.js
@@ -4,18 +4,7 @@ import { GSDevTools } from "gsap/GSDevTools";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 addEventListener("DOMContentLoaded", () => {
-  // gsap.registerPlugin(GSDevTools, ScrollTrigger);
-  // gsap.to(".card-img", {
-  //   duration: 10,
-  //   rotate: 360,
-  //   scrollTrigger: {
-  //     trigger: "#container",
-  //     start: "top 20%",
-  //     end: "bottom",
-  //     markers: true,
-  //     scrub: true,
-  //     pin: ".card-img",
-  //   },
-  // });
-  // GSDevTools.create();
+  gsap.to(".box", {
+    keyframes: [{ x: 100 }, { y: 100 }, { x: 0 }, { y: 0 }],
+  });
 });
