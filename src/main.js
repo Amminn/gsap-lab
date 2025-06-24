0aa8925e326a5b8d6eb8d753dbdef76e7a982b69
Amin <55767303+Amminn@users.noreply.github.com>
2025-06-24 22:50:40 +0100

    feat: add simple effect using blur and stagger

--- src/main.js
+++ src/main.js
@@ -2,9 +2,53 @@ import { gsap } from "gsap";
 
 import { GSDevTools } from "gsap/GSDevTools";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
 
 addEventListener("DOMContentLoaded", () => {
-  gsap.to(".box", {
-    keyframes: [{ x: 100 }, { y: 100 }, { x: 0 }, { y: 0 }],
+  let splitFirstTitle = SplitText.create(".first-title", {
+    type: "chars",
   });
+
+  let splitSecondTitle = SplitText.create(".second-title", {
+    type: "chars",
+  });
+
+  gsap.fromTo(
+    splitFirstTitle.chars,
+    {
+      y: -100,
+    },
+    {
+      y: 100,
+      duration: 1,
+      stagger: {
+        each: 0.05,
+        from: "end",
+      },
+      repeatRefresh: true,
+      ease: "power2.inOut",
+      yoyo: true,
+      repeat: -1,
+    }
+  );
+  gsap.fromTo(
+    splitSecondTitle.chars,
+    {
+      y: -100,
+    },
+    {
+      y: 100,
+      duration: 1,
+      stagger: {
+        each: 0.05,
+        from: "end",
+      },
+      repeatRefresh: true,
+      ease: "power2.inOut",
+      yoyo: true,
+      repeat: -1,
+    }
+  );
 });
